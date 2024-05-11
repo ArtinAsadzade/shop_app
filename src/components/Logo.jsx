be8359bf5fb6baad5f6189230dba3bb6d4dad0a4
ArtinAsadzade <artinasadzade7@gmail.com>
2024-05-11 10:05:40 +0330

    Fix Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,12 @@ import { Link } from "react-router-dom";
 export default function Logo() {
   return (
     <>
-      <Link to="/" className="p-1 cursor-pointer">
+      <Link to="/" className="p-1 cursor-pointer lg:hidden">
+        <h1 className="font-extrabold text-center text-red-600 text-lg lg:text-xl">
+          Articom.ir
+        </h1>
+      </Link>
+      <Link to="/" className="p-1 cursor-pointer hidden lg:block">
         <h1 className="font-extrabold text-red-600 text-lg lg:text-xl">
           Articom.ir
         </h1>
