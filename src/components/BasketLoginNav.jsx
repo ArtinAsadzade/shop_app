7a4a819e053fdc3704924af6d59d2e989cbf1be6
ArtinAsadzade <artinasadzade7@gmail.com>
2024-05-11 10:05:25 +0330

    Fix NavBar

--- src/components/BasketLoginNav.jsx
+++ src/components/BasketLoginNav.jsx
@@ -7,15 +7,15 @@ import {
 export default function BasketLoginNav() {
   return (
     <>
-      <div className="mt-0 mb-3 text-center space-y-5">
-        <div className="cursor-pointer md:flex justify-center align-middle lg:inline-block py-2 px-4 text-sm text-neutral-800 rounded-xl hover:text-neutral-700 hover:bg-red-100 transition">
+      <div className="order-4 lg:flex">
+        <div className="cursor-pointer justify-center lg:justify-start flex items-center h-10 leading-10 px-3 mx-1 transition rounded-xl hover:bg-red-50">
           <UserIcon className="w-5 ml-2 text-red-600" />
-          <p className="text-sm text-slate-600">ورود | ثبت نام</p>
+          <p className="text-sm opacity-95">ورود | ثبت نام</p>
           <ChevronDownIcon className="w-4 text-red-600 mx-1" />
         </div>
-        <div className="cursor-pointer md:flex justify-center align-middle lg:inline-block py-2 px-4 text-sm text-neutral-800 rounded-xl hover:text-neutral-700 hover:bg-red-100 transition">
+        <div className="cursor-pointer justify-center lg:justify-start flex items-center h-10 leading-10 px-3 mx-1 transition rounded-xl hover:bg-red-50">
           <ShoppingCartIcon className="w-5 ml-2 text-red-600" />
-          <p className="text-sm text-slate-600">سبد خرید</p>
+          <p className="text-sm opacity-95">سبد خرید</p>
           <ChevronDownIcon className="w-4 text-red-600 mx-1" />
         </div>
       </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,12 +8,19 @@ export default function Nav() {
   const [navItems] = useState(navData);
   return (
     <>
-      <nav className="fixed z-20 lg:hidden top-0 right-0 bottom-0 flex flex-col w-5/6 max-w-sm py-5 px-6 bg-white border-r overflow-y-auto">
-        <Logo />
+      <nav className="fixed z-20 lg:hidden top-0 right-0 bottom-0 hidden flex-col w-5/6 max-w-sm py-5 px-6 bg-white border-r overflow-y-auto">
         <ul className="space-y-3">
-          <li className="border-b">
+          <li className="border-b w-full  ">
+            <Logo />
             <BasketLoginNav />
           </li>
+          {navItems.map(
+            (item) => !item.active && <NavItems key={item.id} {...item} />
+          )}
+        </ul>
+      </nav>
+      <nav className="hidden lg:flex container">
+        <ul className="flex">
           {navItems.map((item) => (
             <NavItems key={item.id} {...item} />
           ))}
