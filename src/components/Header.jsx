feaf53cff1ba6693a7c825194d1c76fb48f5180a
ArtinAsadzade <artinasadzade7@gmail.com>
2024-05-11 10:05:35 +0330

    Fix Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,20 @@
-import { Bars3Icon } from "@heroicons/react/24/outline";
 import Nav from "./Nav";
 import SearchBar from "./SearchBar";
 import BasketLoginNav from "./BasketLoginNav";
 import Logo from "./Logo";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 export default function Header() {
-  const hamburgerMenuHandler = () => {};
   return (
     <>
       <header className="p-5 container">
         <div className="flex items-center flex-wrap lg:flex-row lg:flex-1 justify-between">
-          <Bars3Icon
-            className="w-7 lg:hidden text-red-600"
-            onClick={hamburgerMenuHandler}
-          />
+          <Bars3Icon className="w-7 lg:hidden text-red-600" />
           <Logo />
           <SearchBar />
-          <BasketLoginNav />
+          <div className="hidden lg:block">
+            <BasketLoginNav />
+          </div>
         </div>
       </header>
       <Nav />
