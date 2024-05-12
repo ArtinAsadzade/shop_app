d0626b75054673c6396c4725da2afb8908261e7b
artin721 <artinasadzade7@gmail.com>
2024-05-12 13:55:51 +0330

    Fix Scroll

--- src/components/ProductOffer.jsx
+++ src/components/ProductOffer.jsx
@@ -1,7 +1,7 @@
 export default function ProductOffer(props) {
   return (
-    props.offerPrice &&
-    props.count && (
+    !!props.offerPrice &&
+    !!props.count && (
       <div className="md:w-1/4 lg:w-1/6 w-1/3 flex-shrink-0 my-5 cursor-pointer mx-3 bg-white bg">
         <img src={props.img} alt="" className=" transition my-3 hover:scale-110" />
         <h1 className="text-center text-md">{props.name}</h1>
