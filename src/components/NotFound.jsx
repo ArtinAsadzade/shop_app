08a389b0eb7a6363c314d54dc4b49dfab609375b
ArtinAsadzade <artinasadzade7@gmail.com>
2024-05-23 22:24:56 +0330

    Update

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,7 @@
 import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
 export default function NotFound() {
   return (
-    <div className="w-full flex items-center">
+    <div className="w-full z-10 flex items-center">
       <h1>محصول مورد نظر شما یافت نشد</h1>
       <ExclamationCircleIcon className="w-6 text-yellow-400" />
     </div>
