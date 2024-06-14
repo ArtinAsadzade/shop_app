9c111da007479269c0e4b2bb9c1638226ce72ee2
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-14 22:00:48 +0330

    Update

--- src/context/UsersDataContext.jsx
+++ src/context/UsersDataContext.jsx
@@ -1,11 +1,11 @@
 import { createContext, useState } from "react";
-import { UsersData } from "../data/data";
 
 export const UsersDataContext = createContext();
 
 export default function UsersDataProvider({ children }) {
-  const [users, setUsers] = useState(UsersData);
-  // JSON.parse(localStorage.getItem("usersData"))
+  const [users, setUsers] = useState(
+    JSON.parse(localStorage.getItem("usersData"))
+  );
 
   return (
     <UsersDataContext.Provider value={{ users, setUsers }}>
