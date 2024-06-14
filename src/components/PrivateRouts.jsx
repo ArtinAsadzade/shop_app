05bd4e59b20e0be1bc930cf788854ee9891666ff
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-14 22:00:34 +0330

    Update

---
+++ src/components/PrivateRouts.jsx
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+import { userLogin } from "../cms/Utils";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+import TopBar from "../cms/components/TopBar";
+import SideBar from "../cms/components/SideBar";
+import { useEffect } from "react";
+
+export default function PrivateRouts() {
+  const { userName, userPassword } = useContext(UserContext);
+  const isLogin = userLogin(userName, userPassword);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!JSON.parse(localStorage.getItem("user"))) {
+      navigate("/");
+    }
+  }, [navigate]);
+
+  return (
+    <>
+      {isLogin ? (
+        <>
+          <TopBar />{" "}
+          <div className="flex mt-[2px]">
+            <SideBar /> <Outlet />
+          </div>
+        </>
+      ) : (
+        <Navigate to={"/"} />
+      )}
+    </>
+  );
+}
