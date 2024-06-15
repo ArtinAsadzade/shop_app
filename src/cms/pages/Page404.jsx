81663af36e14ad94c770291c6fbfef9c486d2e5a
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-15 10:28:51 +0330

    fix

--- src/cms/pages/Page404.jsx
+++ src/cms/pages/Page404.jsx
@@ -6,8 +6,15 @@ export default function Page404() {
     <>
       <div className="w-full h-screen">
         <div className="w-full m-auto text-center p-8">
-          <img src="../../public/Vectors/404Error2.svg" alt="" className="w-full md:w-1/3 mx-auto" />
-          <Link to={"home"} className="border-black border-2 text-black rounded-lg p-2 hover:opacity-50 transition-opacity">
+          <img
+            src="/Vectors/404Error2.svg"
+            alt=""
+            className="w-full md:w-1/3 mx-auto"
+          />
+          <Link
+            to={"home"}
+            className="border-black border-2 text-black rounded-lg p-2 hover:opacity-50 transition-opacity"
+          >
             Back To Home
             <ArrowUpRightIcon className="w-4 inline mx-1" />
           </Link>
