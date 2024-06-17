5a45c8dec438be5d181b0d84e03f1e8a0dc5bd23
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-17 13:15:54 +0330

    Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     container: {
       center: true,
     },
     extend: {
+      fontFamily: {
+        vazir: "yekan",
+      },
     },
   },
-  plugins: [
-],
-}
-
+  plugins: [],
+};
