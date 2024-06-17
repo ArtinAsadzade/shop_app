2ba80d00d250e95340a861c4ce6299ccb2a02643
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-17 13:16:39 +0330

    Update

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,18 +1,10 @@
 import Slider from "../components/HeroSlider";
 import LastOffer from "../components/Product/LastOffer";
 import Offers from "../components/Product/Offers";
-import AmazingOffer from "../components/Product/AmzingOffer";
 import Header from "../components/Header/Header";
 import Footer from "./../components/Footer/Footer";
-import { UsersData, productsData } from "./../data/data";
-import { useEffect } from "react";
 
 export default function Main() {
-  useEffect(() => {
-    localStorage.setItem("usersData", JSON.stringify(UsersData));
-    localStorage.setItem("productsData", JSON.stringify(productsData));
-  }, []);
-
   return (
     <>
       <Header />
@@ -25,9 +17,6 @@ export default function Main() {
         </section>
         <section>
           <Offers />
-        </section>
-        <section>
-          <AmazingOffer />
         </section>
       </main>
       <Footer />
