5f6f34b2e98434024ced36f999214ba92bef37d3
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-18 16:05:41 +0330

    Update

--- src/components/PrivateRouts.jsx
+++ src/components/PrivateRouts.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { userLogin } from "../cms/Utils";
+import { userLogin } from "../Utils";
 import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import TopBar from "../cms/components/TopBar";
