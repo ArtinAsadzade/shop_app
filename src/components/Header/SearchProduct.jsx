5e7eb3556d3da21ebdb7849e01098987cbe5c9bb
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-20 19:02:55 +0330

    Update

--- src/components/Header/SearchProduct.jsx
+++ src/components/Header/SearchProduct.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function SearchProduct(props) {
   return (
-    <Link to={"/"} className="flex items-center bg-white p-2 rounded-xl">
+    <Link to={"/"} className="flex items-center bg-white my-2 p-2 rounded-xl">
       <img className="w-14 rounded-lg ml-2" src={props.img} alt="" />
       <p className="text-xs opacity-70">{props.name}</p>
     </Link>
