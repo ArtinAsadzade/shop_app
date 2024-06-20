2a480042c43f2f4b9384755cdaa58b982c684faa
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-20 19:02:53 +0330

    Update

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -4,7 +4,7 @@ import {
 } from "@heroicons/react/24/outline";
 import useInput from "../../hooks/useInput";
 import { productsData } from "../../data/data";
-import { useMemo, useRef, useState } from "react";
+import { useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import SearchProduct from "./SearchProduct";
 import NotFound from "../NotFound";
@@ -33,7 +33,7 @@ export default function SearchBar() {
       <MagnifyingGlassIcon className="w-5 text-red-600 absolute top-1/2 -translate-y-1/2 left-4" />
       {value.length ? (
         <div className="absolute w-full bg-gray-50 shadow-2xl h-auto mt-2 z-50 rounded-2xl">
-          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-3 p-3">
+          <div className="w-full p-3">
             {search.length ? (
               search.map((item) => <SearchProduct key={item.id} {...item} />)
             ) : (
