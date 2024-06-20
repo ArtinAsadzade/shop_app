fcd333cd9dffe515a41b2f94d2ed6eb5d2ad0f3d
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-20 19:03:05 +0330

    Update

--- src/cms/components/TopBar.jsx
+++ src/cms/components/TopBar.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react";
 import {
-  ArrowLeftStartOnRectangleIcon,
+  ArrowRightStartOnRectangleIcon,
   Bars3Icon,
 } from "@heroicons/react/24/outline";
 import { ShowSideBarContext } from "./../../context/ShowSideBarContext";
@@ -16,48 +16,53 @@ export default function TopBar() {
     setShowSideBar((prevState) => (prevState = !prevState));
   };
 
-  const showLogOutMsg = () => {
+  const openModalHandler = () => {
     setShow((prevState) => (prevState = !prevState));
   };
 
-  const logOutHandler = () => {
-    localStorage.clear();
-    navigate("/");
+  const goToHomePage = () => {
+    navigate("/home");
     setShow((prevState) => (prevState = !prevState));
   };
 
   return (
     <>
       <YesOrNo
-        title={"Log Out!"}
-        desc={`Are You Sure For Log Out?`}
+        title={"Back To Main Page!"}
+        desc={`Are You Sure For Back?`}
         button={true}
-        func={logOutHandler}
+        func={goToHomePage}
         show={show}
         setShow={setShow}
       />
       <div className="flex justify-between p-3 items-center shadow-sm shadow-black">
+        <div className="flex justify-evenly items-center">
+          <ArrowRightStartOnRectangleIcon
+            onClick={openModalHandler}
+            className="w-8 md:w-10 text-black cursor-pointer mx-2"
+          />
+          <div className="flex justify-center px-2 border-r-[1px] items-start flex-col">
+            <img
+              src={
+                JSON.parse(localStorage.getItem("user"))?.profile ||
+                "/Profile/Default.jpg"
+              }
+              alt="Profile Pic"
+              className="bg-black rounded-full w-8 md:w-10 cursor-pointer border-[1px]"
+            />
+            <p className="text-md opacity-95">
+              {JSON.parse(localStorage.getItem("user"))?.firstName}{" "}
+              {JSON.parse(localStorage.getItem("user"))?.lastName}
+            </p>
+          </div>
+        </div>
         <div>
           <h1 className="hidden sm:block font-extrabold text-xl md:text-2xl text-gray-600">
-            articom.ir
+            articom | Admin Panel
           </h1>
           <Bars3Icon
-            className="block sm:hidden w-6 cursor-pointer"
+            className="block sm:hidden w-8 cursor-pointer"
             onClick={showHamburgerHandler}
-          />
-        </div>
-        <div className="flex justify-evenly items-center">
-          <ArrowLeftStartOnRectangleIcon
-            onClick={showLogOutMsg}
-            className="w-5 md:w-8 text-black cursor-pointer mx-2"
-          />
-          <img
-            src={
-              JSON.parse(localStorage.getItem("user"))?.profile ||
-              "/Profile/Default.jpg"
-            }
-            alt="Profile Pic"
-            className="bg-black rounded-full w-8 md:w-10 mx-2 cursor-pointer"
           />
         </div>
       </div>
