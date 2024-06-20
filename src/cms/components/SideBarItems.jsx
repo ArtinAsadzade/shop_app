5c6a222f257c1defd2e36353dee023ea994beee5
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-20 19:02:47 +0330

    Update

--- src/cms/components/SideBarItems.jsx
+++ src/cms/components/SideBarItems.jsx
@@ -2,15 +2,15 @@ import { NavLink } from "react-router-dom";
 
 export default function SideBarItems(props) {
   return (
-    <div className="w-full flex flex-col p-2">
+    <div className="w-full flex items-start flex-col p-2 ">
       <p className="text-gray-600 font-bold text-sm">{props.title}</p>
       {props.infos.map((item) => (
         <NavLink
           key={item.id}
           to={item.link}
           className={({ isActive }) =>
-            `w-full p-1 rounded-md flex items-center justify-start gap-2 ${
-              isActive ? "bg-slate-200" : "hover:bg-slate-100 transition-all"
+            `w-full p-1 rounded-md flex mx-2 px-2 items-center my-1 justify-start gap-2 ${
+              isActive ? "bg-red-200" : "hover:bg-red-100 transition-all"
             }`
           }
         >
