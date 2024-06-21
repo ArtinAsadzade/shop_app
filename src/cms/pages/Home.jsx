0964955eda6e0f6c7de939275f5240690dfe6246
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-21 21:47:09 +0330

    Update

--- src/cms/pages/Home.jsx
+++ src/cms/pages/Home.jsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <>
-      <div className="w-full rounded-sm py-10 px-3"></div>
+      <div className="w-full flex justify-center items-center rounded-sm py-10 px-3">
+        <div className="bg-slate-200 px-5 py-2 text-center rounded-md opacity-50">
+          به پنل ادمین خوش آمدید
+        </div>
+      </div>
     </>
   );
 }
