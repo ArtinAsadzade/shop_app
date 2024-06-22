793a0c4f10b8d54a619f6d706536311fcda9f9a3
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-22 12:54:38 +0330

    Update

--- src/cms/pages/Products.jsx
+++ src/cms/pages/Products.jsx
@@ -1,39 +1,57 @@
 import { PlusIcon } from "@heroicons/react/24/outline";
-import ProductsItem from "../components/ProductsItem";
-import { useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
-import { ProductDataContext } from "./../../context/ProductDataContext";
+import { useContext } from "react";
+import { ProductDataContext } from "../../context/ProductDataContext";
+import ProductItems from "../components/ProductsItem";
 
 export default function Products() {
   const { products } = useContext(ProductDataContext);
+  console.log(products);
 
   return (
-    <div className="w-full min-h-svh bg-slate-200 px-5 py-10">
-      <div className="w-full flex justify-between items-center bg-white m-auto p-3 rounded-lg">
-        <h1 className="font-bold border-b-2">Manage Products</h1>
-        <Link
-          to={"/products/newProduct"}
-          className="p-2 bg-blue-500 mx-3 rounded-lg flex text-white items-center justify-center"
-        >
-          Add Product
-          <PlusIcon className="font-bold w-5 mx-1" />
-        </Link>
-      </div>
-      <div className="w-full m-auto my-10 rounded-lg">
-        <div className="w-full shadow-md rounded-lg grid grid-cols-12 justify-items-center font-bold text-[#7d889e] bg-gray-200 border-b-2 px-10 py-2">
-          <div className="col-span-3 justify-start w-full flex">Image</div>
-          <div className="col-span-3">Name</div>
-          <div className="col-span-3">Price</div>
-          <div className="col-span-3 justify-end w-full flex">Action</div>
+    <>
+      {products && (
+        <div className="w-full min-h-svh bg-slate-100 px-5 py-10 overflow-y-hidden">
+          <div className="w-full flex justify-between items-center bg-white m-auto p-3 rounded-lg">
+            <h1 className="font-bold border-b-2">Manage Members</h1>
+            <Link
+              to={"/users/addNewUser"}
+              className="p-2 bg-blue-500 mx-3 rounded-lg flex text-white items-center justify-center"
+            >
+              Add Product
+              <PlusIcon className="font-bold w-5 mx-1" />
+            </Link>
+          </div>
+          <div className="w-full m-auto my-10 rounded-lg">
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+              <table className="w-full text-sm text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                  <tr>
+                    <th scope="col" className="px-6 py-3 text-right">
+                      عکس
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-right">
+                      نام
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-center">
+                      قیمت نقش
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-left">
+                      عمل
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products?.map((item) => (
+                    <ProductItems key={item.id} {...item} />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <Outlet />
         </div>
-
-        <div>
-          {products.map((item) => (
-            <ProductsItem key={item.id} {...item} />
-          ))}
-        </div>
-      </div>
-      <Outlet />
-    </div>
+      )}
+    </>
   );
 }
