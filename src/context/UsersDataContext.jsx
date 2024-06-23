640c594b15726b54b58c3c9a10e9b914995af018
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-23 13:59:38 +0330

    Update

--- src/context/UsersDataContext.jsx
+++ src/context/UsersDataContext.jsx
@@ -1,11 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import useDecrypted from "../hooks/useDecrypted";
 
 export const UsersDataContext = createContext();
 
 export default function UsersDataProvider({ children }) {
-  const [users, setUsers] = useState(
-    JSON.parse(localStorage.getItem("usersData"))
-  );
+  const [users, setUsers] = useState(null);
+
+  const [decryptedUsers] = useDecrypted("usersData");
+
+  useEffect(() => {
+    setUsers(decryptedUsers);
+    console.log(users);
+  }, [decryptedUsers]);
 
   return (
     <UsersDataContext.Provider value={{ users, setUsers }}>
