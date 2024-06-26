abb6cc31ac9c5d8cd6bb1863b63bd630531be755
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-26 22:59:19 +0330

    Update animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,32 @@ export default {
       fontFamily: {
         vazir: "yekan",
       },
+      keyframes: {
+        "slide-in-right": {
+          "0%": {
+            transform: "translateX(100%)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "translateX(0)",
+            opacity: "1",
+          },
+        },
+        "slide-out-right": {
+          "0%": {
+            transform: "translateX(0)",
+            opacity: "1",
+          },
+          "100%": {
+            transform: "translateX(100%)",
+            opacity: "0",
+          },
+        },
+      },
+      animation: {
+        "slide-in-right": "slide-in-right 0.5s ease-out forwards",
+        "slide-out-right": "slide-out-right 0.5s ease-out forwards",
+      },
     },
   },
   plugins: [],
