dc12687669bb8fe11a628639380b7453cd945ed1
ArtinAsadzade <artinasadzade7@gmail.com>
2024-06-28 17:06:37 +0330

    Update

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Product(props) {
   return (
-    <div className="col-span-12 cursor-pointer sm:col-span-6 md:col-span-4 lg:col-span-3 2xl:col-span-2 rounded-lg mb-10 mx-3 p-3 bg-white shadow-lg">
+    <div className="col-span-12 cursor-pointer hover:scale-105 hover:shadow-2xl transition-all sm:col-span-6 md:col-span-4 lg:col-span-3 2xl:col-span-2 rounded-lg mb-10 mx-3 p-3 bg-white shadow-lg">
       <div className="w-full p-5">
         <img src={props.img} alt="" className="w-full object-cover" />
       </div>
