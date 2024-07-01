f260d885cd143bb716e58f2dbb610375230c5c7a
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-01 09:33:45 +0330

    Update

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -20,25 +20,28 @@ export default function Nav({ showNavBar, hamburgerHandler }) {
     <>
       {/* Mobile Nav */}
       <div
-        className={`fixed inset-0 transition-all duration-300 bg-black bg-opacity-10 backdrop-blur-sm ${
-          showNavBar ? "block" : "invisible opacity-0"
-        }`}
+        className={`fixed inset-0 transition-all duration-300 z-50 bg-black bg-opacity-10 backdrop-blur-sm ${
+          showNavBar ? "" : "invisible opacity-0"
+        } ${showNavBar ? "block" : "invisible opacity-0"}`}
         onClick={hamburgerHandler}
       ></div>
       <nav
-        className={`fixed transition-all duration-300 z-50 lg:hidden top-0 right-0 bottom-0 flex-col w-5/6 max-w-sm py-5 px-6 bg-white border-r overflow-y-auto ${
+        className={`fixed transition-all duration-300 z-50 lg:hidden top-0 right-0 bottom-0 flex-col w-1/2 max-w-sm py-5 px-6 bg-white border-r overflow-y-auto ${
           showNavBar ? "flex visible opacity-100" : "invisible opacity-0 translate-x-96"
         }`}
       >
         <ul className="space-y-3">
-          <XMarkIcon className="w-7 cursor-pointer text-red-600" onClick={hamburgerHandler} />
-          <li className="border-b w-full">
+          <li className="flex justify-between">
             <Logo />
+            <XMarkIcon className="w-7 cursor-pointer text-red-600" onClick={hamburgerHandler} />
+          </li>
+          <li className="border-b w-full py-2">
             <BasketLoginNav />
           </li>
           {navItems.map((item) => !item.active && <NavItems key={item.id} {...item} />)}
         </ul>
       </nav>
+
       {/* Lg Nav */}
       <nav className="hidden lg:flex container">
         <ul className="flex">
