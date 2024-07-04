b40b91f139b53668c61880bee291f0c351e02c3f
ArtinAsadzade <artinasadzade7@gmail.com>
2024-07-04 22:39:10 +0330

    Fix

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -1,7 +1,4 @@
-import {
-  ChevronLeftIcon,
-  MagnifyingGlassIcon,
-} from "@heroicons/react/24/outline";
+import { ChevronLeftIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import useInput from "../../hooks/useInput";
 import { productsData } from "../../data/data";
 import { useMemo, useState } from "react";
@@ -14,9 +11,7 @@ export default function SearchBar() {
   const [products] = useState(productsData);
   const search = useMemo(() => {
     if (value.length) {
-      return products
-        .filter((item) => item.name.includes(value.toLowerCase()))
-        .slice(0, 3);
+      return products.filter((item) => item.name.includes(value.toLowerCase())).slice(0, 3);
     }
   }, [products, value]);
 
@@ -32,21 +27,11 @@ export default function SearchBar() {
       />
       <MagnifyingGlassIcon className="w-5 text-red-600 absolute top-1/2 -translate-y-1/2 left-4" />
       {value.length ? (
-        <div className="absolute w-full bg-gray-50 shadow-2xl h-auto mt-2 z-50 rounded-2xl">
-          <div className="w-full p-3">
-            {search.length ? (
-              search.map((item) => <SearchProduct key={item.id} {...item} />)
-            ) : (
-              <NotFound />
-            )}
-          </div>
+        <div className="absolute w-full h-full bg-white shadow-lg border-[1px] mt-2 z-50 rounded-2xl">
+          <div className="w-full h-full p-3">{search.length ? search.map((item) => <SearchProduct key={item.id} {...item} />) : <NotFound />}</div>
           <div className="flex flex-wrap gap-2 px-3 mb-3">
             {search.map((item) => (
-              <Link
-                to=""
-                className="bg-white rounded-xl px-2 py-1 flex items-center text-sm"
-                key={item.id}
-              >
+              <Link to={`/shop/product${item.id}`} className="bg-white rounded-xl px-2 py-1 flex items-center text-sm" key={item.id}>
                 {item.name} <ChevronLeftIcon className="w-3 mx-1" />
               </Link>
             ))}
