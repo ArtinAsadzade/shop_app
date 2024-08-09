eb793b914d80d3b730f62609e3a54b74e0593eae
ArtinAsadzade <artinasadzade7@gmail.com>
2024-08-09 15:01:03 +0330

    Update

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,10 +4,10 @@ export default function Logo() {
   return (
     <>
       <Link to="/" className="p-1 cursor-pointer lg:hidden">
-        <h1 className="font-extrabold text-center text-primary text-lg lg:text-xl">Articom.ir</h1>
+        <h1 className="font-extrabold text-center text-primary text-lg lg:text-xl">Artin Asadzade.ir</h1>
       </Link>
       <Link to="/" className="p-1 cursor-pointer hidden lg:block">
-        <h1 className="font-extrabold text-primary text-lg lg:text-xl">Articom.ir</h1>
+        <h1 className="font-extrabold text-primary text-lg lg:text-xl">Artin Asadzade.ir</h1>
       </Link>
     </>
   );
